00839ea3be70a0f376b75824a80335aff1ab1ff1
DanielBalan <danni_balan@yahoo.com>
2021-02-15 19:58:59 +0200

    Adaugare comment "Java - Tortoise"

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,7 +1,7 @@
 package ro.java.ctrln;
 
 public class Main {
-
+    //JAVA - Tortoise
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
